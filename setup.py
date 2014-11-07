bf32c7085951dd7d1f9491e48a9f946d0474887b
Giovani Chiachia <giovanichiachia@gmail.com>
2014-11-07 10:55:18 -0200

    minor fix in dependency name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=['numpy>=1.6.1',
                       'scipy>=0.10.0',
                       'numexpr>=2.0',
-                      'scikits-image>=0.5',
+                      'scikit-image>=0.5',
                       'nose>=1.3.0'],
     package_data={'': ['*.md']},
 )
